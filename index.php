c099fb5011225bc2a6aad9c13a17da734bc504be
valerio <valerio.deprosperi@gmail.com>
2020-10-05 15:48:37 +0200

    init

---
+++ index.php
@@ -0,0 +1,22 @@
+<!-- Creiamo un array 'matches' contenente altri array i quali rappresentano delle partite di basket di un’ipotetica tappa del calendario.
+Ogni array della partita avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. -->
+<!-- Stampiamo a schermo tutte le partite con questo schema: -->
+<!-- Olimpia Milano - Cantù | 55-60 -->
+<?php
+  $matches = [
+
+      [
+        "casa" => "Roma",
+        "ospite" => "Milano",
+        "punti_casa" => 34,
+        "punti_ospiti" => 31,
+      ],
+      [
+        
+      ]
+
+  ];
+
+
+
+ ?>
